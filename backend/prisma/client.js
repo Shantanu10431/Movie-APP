c5f3d5b55c2fc7252b58c5cdf0ad06653b5271c8
Shantanu <shantanuk10431@gmail.com>
2026-02-19 15:48:15 +0530

    fix(backend): initialize prisma with explicit process.env.DATABASE_URL

--- backend/prisma/client.js
+++ backend/prisma/client.js
@@ -1,5 +1,13 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+console.log("Checking DATABASE_URL...", process.env.DATABASE_URL ? "Defined" : "UNDEFINED");
+
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL,
+        },
+    },
+});
 
 module.exports = prisma;
